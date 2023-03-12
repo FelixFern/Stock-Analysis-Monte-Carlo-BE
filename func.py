32aa42b00f8e0c8ce27237eca881577308a83bdd
Felix Fernando <felixzhuang1@gmail.com>
2023-03-12 15:35:32 +0700

    feat: added generate_decision_sequence function and small fix on other function

--- func.py
+++ func.py
@@ -7,11 +7,11 @@ import pandas as pd
 
 # Data manipulation
 import numpy as np
-import re # Cleaning texts
+import re  # Cleaning texts
 import time
-import datetime as dt # Datetime manipulation
-from scipy.stats import pearsonr # Statistics
-from scipy.optimize import minimize # Minimze SR
+import datetime as dt  # Datetime manipulation
+from scipy.stats import pearsonr  # Statistics
+from scipy.optimize import minimize  # Minimze SR
 
 # Plotting
 import seaborn as sns
@@ -21,11 +21,12 @@ plt.style.use('ggplot')
 # ------------------------------------------------------------------------------------------------- #
 # Fetch Data
 
+
 def fetch_data(stock, start_date, end_date):
-    data = yf.download(stock, 
-                       start = start_date, 
-                       end = end_date, 
-                       progress = False)
+    data = yf.download(stock,
+                       start=start_date,
+                       end=end_date,
+                       progress=False)
 
     data = data.reset_index()
     return data
@@ -33,11 +34,12 @@ def fetch_data(stock, start_date, end_date):
 # ------------------------------------------------------------------------------------------------- #
 # Data Manipulation
 
+
 def check_columns(data):
     for col in data.columns:
         if col == 'Date':
             if data[col].dtype == 'str':
-                data[col] =  pd.to_datetime(data[col]).date()
+                data[col] = pd.to_datetime(data[col]).date()
 
         else:
             data[col] = data[col].astype('float64')
@@ -49,10 +51,10 @@ def check_values(data):
     cols = [col for col in data.columns if data[col].dtype == 'float64']
 
     # Null values
-    data = data.dropna(subset = cols)
+    data = data.dropna(subset=cols)
 
     # Negative values
-    data = data[(data[cols] > 0).all(1)].reset_index(drop = True)
+    data = data[(data[cols] > 0).all(1)].reset_index(drop=True)
 
     return data
 
@@ -60,18 +62,18 @@ def check_values(data):
 def expand_data(data, start_date, end_date):
     # Create temporary dataframe
     temp = pd.DataFrame()
-    temp['Date'] = pd.date_range(start = start_date, 
-                                 end = end_date)
+    temp['Date'] = pd.date_range(start=start_date,
+                                 end=end_date)
 
     # Merge the data
-    res = pd.merge(temp, data, how = 'left', on = 'Date')
-    
+    res = pd.merge(temp, data, how='left', on='Date')
+
     # Interpolate the missing values
     # IDR/USD dataset
     a = res.rolling(3).mean()
     b = res.iloc[::-1].rolling(3).mean()
 
-    c = a.fillna(b).fillna(res).interpolate(method = 'nearest').ffill().bfill()
+    c = a.fillna(b).fillna(res).interpolate(method='nearest').ffill().bfill()
 
     res = res.fillna(c)
 
@@ -80,29 +82,31 @@ def expand_data(data, start_date, end_date):
 # ------------------------------------------------------------------------------------------------- #
 # Final Data Preparation
 
+
 def final_data(data, start_date, end_date, criteria):
     # Check columns type
-    data = check_columns(data = data)
+    data = check_columns(data=data)
 
     # Check null and negative values
-    data = check_values(data = data)
+    data = check_values(data=data)
 
     # Expand and interpolate
-    data = expand_data(data = data, start_date = start_date, end_date = end_date)
+    data = expand_data(data=data, start_date=start_date, end_date=end_date)
 
     data = data.set_index('Date')
-    
+
     return data[[criteria]]
 
 
 def get_return(data):
-    data['Daily Return'] = (data.iloc[:, 0] - data.iloc[:, 0].shift(1)) / data.iloc[:, 0].shift(1)
+    data['Daily Return'] = (
+        data.iloc[:, 0] - data.iloc[:, 0].shift(1)) / data.iloc[:, 0].shift(1)
     data = data.dropna()
 
-    sns.displot(data['Daily Return'].dropna(), 
-                bins = 50,
-                color = 'blue', 
-                kde = True)
+    sns.displot(data['Daily Return'].dropna(),
+                bins=50,
+                color='blue',
+                kde=True)
     plt.title('Daily return distribution')
     plt.show()
 
@@ -111,7 +115,8 @@ def get_return(data):
 # ------------------------------------------------------------------------------------------------- #
 # Monte Carlo Simulation
 
-def simulation(data, days, n_sim):   
+
+def simulation(data, days, n_sim):
     start_price = data.iloc[-1, 0]
     sim = np.zeros(n_sim)
     table = np.zeros((n_sim, days))
@@ -119,28 +124,30 @@ def simulation(data, days, n_sim):
     delta = 1 / days
     mu = data['Daily Return'].mean()
     sigma = data['Daily Return'].std()
-    def monte_carlo(start_price, days, mu = mu, sigma = sigma):
+
+    def monte_carlo(start_price, days, mu=mu, sigma=sigma):
         price = np.zeros(days)
         price[0] = start_price
-        
+
         shock = np.zeros(days)
         drift = np.zeros(days)
-        
+
         for x in range(1, days):
-            shock[x] = np.random.normal(loc = mu * delta, scale = sigma * np.sqrt(delta))
+            shock[x] = np.random.normal(
+                loc=mu * delta, scale=sigma * np.sqrt(delta))
             drift[x] = mu * delta
-            
+
             price[x] = price[x - 1] + (price[x - 1] * (drift[x] + shock[x]))
-        
+
         return price
 
-    plt.figure(figsize = (15, 8))
+    plt.figure(figsize=(15, 8))
     for i in range(n_sim):
         result = monte_carlo(start_price, days)
         table[i] = result
         sim[i] = result[days - 1]
         plt.plot(result)
-        
+
     plt.xlabel('Days')
     plt.ylabel('Price')
     plt.title('Monte Carlo Analysis')
@@ -149,6 +156,7 @@ def simulation(data, days, n_sim):
 
 # ------------------------------------------------------------------------------------------------- #
 # Trading Algo and Simulation
+
 
 def trading_algo(mrx):
     '''
@@ -190,7 +198,7 @@ def trading_algo(mrx):
             # Check decreasing
             else:
                 while cond:
-                    
+
                     # Still decreasing and not at the end
                     if (mrx[i, j] < mrx[i, j - 1]) & (j != mrx.shape[1] - 1):
                         j = j + 1
@@ -221,41 +229,80 @@ def trading_sim(price, decision, money):
     res = np.zeros(price.shape[0])
     n_sim = price.shape[0]
     days = price.shape[1]
+    result_df = pd.DataFrame(columns=['SIMULATION', 'PROFIT/LOSS',
+                             'FINAL_BALANCE', 'STARTING_BALANCE'], index=[i for i in range(n_sim)])
 
     for i in range(n_sim):
         # Initial stock at hand
-        stock = money / price[i, 0]
-        
+        stock = 0
+        curr_money = money
+
         for j in range(days):
             # Hold
-            if decision[i, j] == 0:
+            if decision['DECISION'].iloc[j] == 0:
                 pass
 
             # Sell
-            elif decision[i, j] == 1:
+            elif decision['DECISION'].iloc[j] == 1:
                 # Proportion of sell decision at given day
-                arr = price[i, :]
-                factor = (arr == 1).sum()
-
+                factor = decision['SELL_CONF'].iloc[j] / \
+                    (decision['SELL_CONF'].iloc[j] +
+                     decision['BUY_CONF'].iloc[j])
                 curr_price = price[i, j]
-                money = money + factor * stock * curr_price
-                stock = (1 - factor) * stock
-
+                curr_money += stock * factor * curr_price
+                stock -= stock * factor
             # Buy
             else:
                 # Proportion of buy decision at given day
-                arr = price[i, :]
-                factor = (arr == 2).sum()
-
+                factor = decision['BUY_CONF'].iloc[j] / \
+                    (decision['SELL_CONF'].iloc[j] +
+                     decision['BUY_CONF'].iloc[j])
                 curr_price = price[i, j]
-                money = (1 - factor) * money
-                stock = stock + factor * money / curr_price
+                stock += factor * curr_money / curr_price
+                curr_money -= factor * curr_money
 
         # Final day result
-        res[i] = money + stock * price[i, -1]
+        res[i] = curr_money + stock * price[i, -1]
+
+        result_df['SIMULATION'].iloc[i] = i + 1
+        result_df['FINAL_BALANCE'].iloc[i] = curr_money + stock * price[i, -1]
+        result_df['STARTING_BALANCE'].iloc[i] = money
 
     pct_return = 100 * (res - money) / money
-    
-    return pct_return
+    result_df['PROFIT/LOSS'] = pct_return
+
+    return result_df
 
 # ------------------------------------------------------------------------------------------------- #
+
+
+def generate_decision_sequence(data, n_sim, days):
+    decision_df = pd.DataFrame(columns=[
+                               'DAY', 'DECISION', 'HOLD_CONF', 'BUY_CONF', 'SELL_CONF'], index=[i for i in range(days)])
+
+    for i in range(days):
+        decision_df['DAY'].iloc[i] = i + 1
+        buy = 0
+        sell = 0
+        hold = 0
+
+        for j in range(n_sim):
+            if data[j, i] == 0:
+                hold += 1
+            elif data[j, i] == 1:
+                sell += 1
+            else:
+                buy += 1
+
+        if max(buy, sell, hold) == hold and (hold / n_sim) > 0.5:
+            decision_df['DECISION'].iloc[i] = 0
+        elif max(buy, sell) == sell:
+            decision_df['DECISION'].iloc[i] = 1
+        else:
+            decision_df['DECISION'].iloc[i] = 2
+
+        decision_df['HOLD_CONF'].iloc[i] = hold / n_sim
+        decision_df['BUY_CONF'].iloc[i] = buy / n_sim
+        decision_df['SELL_CONF'].iloc[i] = sell / n_sim
+
+    return decision_df
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import modules
 # Extract stock market data
+from func import fetch_data, check_columns, check_values, expand_data, final_data, get_return, simulation, trading_algo, trading_sim
 import yfinance as yf
 
 # Importing and transforming file
@@ -7,11 +8,11 @@ import pandas as pd
 
 # Data manipulation
 import numpy as np
-import re # Cleaning texts
+import re  # Cleaning texts
 import time
-import datetime as dt # Datetime manipulation
-from scipy.stats import pearsonr # Statistics
-from scipy.optimize import minimize # Minimze SR
+import datetime as dt  # Datetime manipulation
+from scipy.stats import pearsonr  # Statistics
+from scipy.optimize import minimize  # Minimze SR
 
 # Plotting
 import seaborn as sns
@@ -19,7 +20,6 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 # Functions
-from func import fetch_data, check_columns, check_values, expand_data, final_data, get_return, simulation, trading_algo, trading_sim
 
 # ------------------------------------------------------------------------------------------------- #
 # Main Features
@@ -29,24 +29,25 @@ start_date = '2021-01-01'
 end_date = dt.datetime.now().strftime('%Y-%m-%d')
 
 # Stock
-stock = fetch_data(stock = 'ANTM.JK',
-                   start_date = start_date, 
-                   end_date = end_date)
-stock = final_data(data = stock, start_date = start_date, end_date = end_date, criteria = 'Adj Close')
+stock = fetch_data(stock='ANTM.JK',
+                   start_date=start_date,
+                   end_date=end_date)
+stock = final_data(data=stock, start_date=start_date,
+                   end_date=end_date, criteria='Adj Close')
 
 # Overview
 get_return(stock)
 
-# Simulation 
+# Simulation
 n_sim = 1000
 days = 60
 
 # Simulation
-price = simulation(data = stock, days = days, n_sim = n_sim)
+price = simulation(data=stock, days=days, n_sim=n_sim)
 
 # Trading
 money = 1e6
-decision = trading_algo(mrx = price)
-final_sim = trading_sim(price = price, decision = decision, money = money)
+decision = trading_algo(mrx=price)
+final_sim = trading_sim(price=price, decision=decision, money=money)
 
 print(final_sim)
